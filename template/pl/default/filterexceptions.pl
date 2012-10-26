4c0c1ace8e9f5ed7eb94901d380579f60cd19d18
kkurzawski <kkurzawski@aviary.pl>
2012-10-26 21:30:18 +0200

    !l10n Rev 8219 Bug 616191: Implement UI to easily tag bugs from the bug report directly (and get rid of the current form in the footer)

--- template/pl/default/filterexceptions.pl
+++ template/pl/default/filterexceptions.pl
@@ -178,10 +178,6 @@
   'series.frequency * 2',
 ],
 
-'global/per-bug-queries.html.tmpl' => [
-  '" value=\"$bugids\"" IF bugids',
-],
-
 'global/select-menu.html.tmpl' => [
   'options', 
   'size', 
