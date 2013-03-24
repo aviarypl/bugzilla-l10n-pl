1cb4691cc8b6863c89c3df1f822db8178a1ea40e
kkurzawski <kkurzawski@aviary.pl>
2013-03-24 21:39:35 +0100

    Bugzilla 4.4rc2

--- extensions/Example/template/pl/default/setup/strings.txt.pl
+++ extensions/Example/template/pl/default/setup/strings.txt.pl
@@ -1,4 +1,4 @@
-# 4.4rc1+PL@aviary.pl
+# 4.4rc2PL@aviary.pl
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
