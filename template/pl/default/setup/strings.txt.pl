62089b1fd5d8403d1538a3b4b841ed2f32cdf950
kkurzawski <kkurzawski@aviary.pl>
2013-04-23 15:23:58 +0200

    Rev 7749 Bug 854074: Remove all references to the uwinnipeg.ca PPM repository as it is no longer available

--- template/pl/default/setup/strings.txt.pl
+++ template/pl/default/setup/strings.txt.pl
@@ -155,24 +155,6 @@ EOT
     module_ok => 'ok',
     module_unknown_version => "znaleziono nieznana wersje",
     no_such_module => "W repozytorium CPAN nie znaleziono modulu Perl o nazwie ##module##.",
-    ppm_repo_add => <<EOT,
-***********************************************************************
-* Informacja dla uzytkownikow systemu Windows                         *
-***********************************************************************
-* Aby zainstalowac wymienione ponizej moduly, nalezy najpierw jako    * 
-* Administrator uruchomic nastepujace polecenie:                      *
-*                                                                     *
-*   ppm repo add theory58S ##theory_url##
-EOT
-    ppm_repo_up => <<EOT,
-*                                                                     *
-* Nastepnie (rowniez jako Administrator) nalezy wykonac:              *
-*                                                                     *
-*   ppm repo up theory58S                                             *
-*                                                                     *
-* Ostatnie polecenie nalezy uruchamiac az do chwili ujrzenia na       *
-* poczatku wyswietlanej listy tekstu "theory58S".                     *
-EOT
     template_precompile   => "Kompilowanie szablonow... ",
     template_removal_failed => <<END,
 OSTRZEZENIE: Nie mozna usunac katalogu '##datadir##/template'.
